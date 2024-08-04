97fc07bf5616501a08ff4fa03d07ddd478dcf256
unknown <psegura@miumg.edu.gt>
2024-08-03 21:16:36 -0600

    Tarea 1 java

---
+++ src/calculadorap/CalculadoraP.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorap;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CalculadoraP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void CalculadoraP(String[] args) {
+        frm_CalculadoraP frm = new frm_CalculadoraP();
+        frm.show();
+    }
+    
+}
